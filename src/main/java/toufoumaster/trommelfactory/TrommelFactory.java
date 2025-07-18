98ede3c03f76012f4caabe7e98c190dbe817e458
ToufouMaster <owen.kala@gmx.fr>
2025-07-18 17:02:15 +0200

    Gradle fix

--- src/main/java/toufoumaster/trommelfactory/TrommelFactory.java
+++ src/main/java/toufoumaster/trommelfactory/TrommelFactory.java
@@ -11,7 +11,7 @@ public class TrommelFactory implements ModInitializer, GameStartEntrypoint {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("TrommelFactory initialized.");
     }
 
 	@Override
